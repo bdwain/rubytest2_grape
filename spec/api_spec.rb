9801d90714c3d52d4e80ee2252629ead13ca8e97
Bryan Wain <bryandwain@gmail.com>
2013-11-26 05:13:32 -0600

    add tests for the 3 get endpoints

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -93,4 +93,85 @@ describe RecordParser::API do
       end
     end
   end
+
+  describe "GET /records/gender" do
+    before(:each) do
+      File.open(record_filename, "w") do |file|
+        file.puts "Smith Bob Male 10/12/1985 Blue"
+        file.puts "Jones Sarah Female 12/12/1982 Blue"
+        file.puts "Jones Tom Male 1/12/1984 Green"
+        file.puts "Obama Michelle Female 1/13/1964 Red"
+        file.puts "Obama Barack Male 8/4/1962 Blue"
+      end
+      get "/records/gender"
+    end
+
+    it "returns an HTTP 200 response" do
+      expect(last_response.status).to eq(200)
+    end
+
+    it "returns the records sorted by gender (female first) and then last name ascending" do
+      result = JSON.parse(last_response.body)
+      expect(result.length).to eq(5)
+      expect(result[0]["last_name"] == "Jones" && result[0]["first_name"] == "Sarah").to be_true
+      expect(result[1]["last_name"] == "Obama" && result[1]["first_name"] == "Michelle").to be_true
+      expect(result[2]["last_name"] == "Jones" && result[2]["first_name"] == "Tom").to be_true
+      expect(result[3]["last_name"] == "Obama" && result[3]["first_name"] == "Barack").to be_true
+      expect(result[4]["last_name"] == "Smith" && result[4]["first_name"] == "Bob").to be_true
+    end
+  end
+
+  describe "GET /records/birthdate" do
+    before(:each) do
+      File.open(record_filename, "w") do |file|
+        file.puts "Smith Bob Male 10/12/1985 Blue"
+        file.puts "Jones Sarah Female 12/12/1982 Blue"
+        file.puts "Jones Tom Male 1/12/1984 Green"
+        file.puts "Obama Michelle Female 1/13/1964 Red"
+        file.puts "Obama Barack Male 8/4/1962 Blue"
+      end
+      get "/records/birthdate"
+    end
+
+    it "returns an HTTP 200 response" do
+      expect(last_response.status).to eq(200)
+    end
+
+    it "returns the records sorted by birth date ascending" do
+      result = JSON.parse(last_response.body)
+      expect(result.length).to eq(5)
+      expect(result[0]["last_name"] == "Obama" && result[0]["first_name"] == "Barack").to be_true
+      expect(result[1]["last_name"] == "Obama" && result[1]["first_name"] == "Michelle").to be_true
+      expect(result[2]["last_name"] == "Jones" && result[2]["first_name"] == "Sarah").to be_true
+      expect(result[3]["last_name"] == "Jones" && result[3]["first_name"] == "Tom").to be_true
+      expect(result[4]["last_name"] == "Smith" && result[4]["first_name"] == "Bob").to be_true
+    end
+  end
+
+  describe "GET /records/name" do
+    before(:each) do
+      File.open(record_filename, "w") do |file|
+        file.puts "Smith Bob Male 10/12/1985 Blue"
+        file.puts "Jones Sarah Female 12/12/1982 Blue"
+        file.puts "Walsh Tom Male 1/12/1984 Green"
+        file.puts "Roosevelt Eleanor Female 1/13/1964 Red"
+        file.puts "Washington George Male 2/4/1934 Blue"
+      end
+      get "/records/name"
+    end
+
+    it "returns an HTTP 200 response" do
+      expect(last_response.status).to eq(200)
+    end
+
+    it "returns the records sorted by last name descending" do
+      result = JSON.parse(last_response.body)
+      expect(result.length).to eq(5)
+      expect(result[0]["last_name"] == "Washington" && result[0]["first_name"] == "George").to be_true
+      expect(result[1]["last_name"] == "Walsh" && result[1]["first_name"] == "Tom").to be_true
+      expect(result[2]["last_name"] == "Smith" && result[2]["first_name"] == "Bob").to be_true
+      expect(result[3]["last_name"] == "Roosevelt" && result[3]["first_name"] == "Eleanor").to be_true
+      expect(result[4]["last_name"] == "Jones" && result[4]["first_name"] == "Sarah").to be_true
+    end
+  end
 end
