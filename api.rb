6f46c30c61754eb08a45e7f6472ef6e36a273138
Bryan Wain <bryandwain@gmail.com>
2013-11-26 00:25:13 -0600

    set up endpoints for each API method

--- api.rb
+++ api.rb
@@ -3,7 +3,6 @@ require_relative 'lib/record_set'
 
 module RecordParser
   class API < Grape::API
-
     version 'v1', using: :path
     format :json
 
@@ -11,6 +10,25 @@ module RecordParser
     end
 
     resource :records do
+      desc "Post a single data line delimited by either pipes, commas, or spaces"
+      post do
+
+      end
+
+      desc "Return records sorted by gender (females before males) then by last name ascending."
+      get :gender do
+
+      end
+
+      desc "Return records sorted by birth date, ascending."
+      get :birthdate do
+
+      end
+
+      desc "Return records sorted by last name, descending."
+      get :name do
+
+      end
     end
   end
 end
