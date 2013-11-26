885044e44efb9a32bd50f9f22c954acbc3a9e2ee
Bryan Wain <bryandwain@gmail.com>
2013-11-25 20:58:50 -0600

    added get_records_by_birth_date

--- lib/record_set.rb
+++ lib/record_set.rb
@@ -9,4 +9,19 @@ class RecordSet
     @records.concat(newRecords.select {|record| record.valid? })
   end
 
+  def get_records_by_gender
+    records.sort do |r1, r2|
+      if r1.gender == r2.gender
+        r1.last_name <=> r2.last_name
+      else
+        r1.gender <=> r2.gender
+      end
+    end
+  end
+
+  def get_records_by_birth_date
+    records.sort do |r1, r2|
+      r1.date_of_birth <=> r2.date_of_birth
+    end
+  end
 end
--- spec/record_set_spec.rb
+++ spec/record_set_spec.rb
@@ -29,4 +29,75 @@ describe RecordSet do
       expect(set.records.include?(record3)).to be_false
     end
   end
+
+  describe "get_records_by_gender" do
+    let(:bob_smith) do
+      record = Record.new
+      record.set_values_manually("Smith", "Bob", "Male", "Blue", "5/15/1988")
+      return record
+    end
+
+    let(:sarah_jones) do
+      record = Record.new
+      record.set_values_manually("Jones", "Sarah", "Female", "Green", "1/2/1990")
+      return record
+    end
+
+    let(:will_jones) do
+      record = Record.new
+      record.set_values_manually("Jones", "Will", "Male", "Red", "1/3/1945")
+      return record
+    end    
+
+    let(:records) do
+      records = RecordSet.new
+      records.add_records([bob_smith, sarah_jones, will_jones])
+      records.get_records_by_gender
+    end
+
+    it "returns an array with all of the added records" do
+      expect(records.length).to eq(3)
+      expect(records.include?(bob_smith)).to be_true
+      expect(records.include?(sarah_jones)).to be_true
+      expect(records.include?(will_jones)).to be_true
+    end
+
+    it "puts females before males in the results" do
+      expect(records[0]).to eq(sarah_jones)
+    end
+
+    it "sorts by last name ascending when the gender is the same" do
+      expect(records[1]).to eq(will_jones)
+    end
+  end
+
+  describe "get_records_by_birth_date" do
+    let(:young_person) do
+      record = Record.new
+      record.set_values_manually("Smith", "Bob", "Male", "Blue", "5/15/1988")
+      return record
+    end
+
+    let(:old_person) do
+      record = Record.new
+      record.set_values_manually("Jones", "Will", "Male", "Red", "1/3/1945")
+      return record
+    end    
+
+    let(:records) do
+      records = RecordSet.new
+      records.add_records([young_person, old_person])
+      records.get_records_by_birth_date
+    end
+
+    it "returns an array with all of the added records" do
+      expect(records.length).to eq(2)
+      expect(records.include?(old_person)).to be_true
+      expect(records.include?(young_person)).to be_true
+    end
+
+    it "puts the earlier dates of birth first" do
+      expect(records[0]).to eq(old_person)
+    end
+  end
 end
