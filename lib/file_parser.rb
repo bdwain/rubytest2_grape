2d894b600b9331808f05cdf96f4fd9b56dbe9875
Bryan Wain <bryandwain@gmail.com>
2013-11-26 02:27:07 -0600

    rescue exceptions in parse_file when the file does not exist

--- lib/file_parser.rb
+++ lib/file_parser.rb
@@ -3,12 +3,15 @@ require_relative 'record'
 class FileParser
   def parse_file(file_path)
     result_set = Array.new
-    File.open(file_path) do |file|
-      file.each_line do |line|
-        record = Record.new
-        record.parse(line)
-        result_set.push(record)
+    begin
+      File.open(file_path) do |file|
+        file.each_line do |line|
+          record = Record.new
+          record.parse(line)
+          result_set.push(record)
+        end
       end
+    rescue Errno::ENOENT
     end
     result_set
   end
--- spec/file_parser_spec.rb
+++ spec/file_parser_spec.rb
@@ -3,28 +3,36 @@ require 'tempfile'
 
 describe FileParser do
   describe "parse_file" do
-    it "returns a set of records with the contents of the file" do
-      file_parser = FileParser.new
-      file = Tempfile.new('FileParser_parse_file_test')
-      begin
-        file.puts("Jones Sarah Female 4/1/1982 Red")
-        file.puts("Smith Joe Male 5/15/1988 Blue")
-        file.rewind
-        result_set = file_parser.parse_file(file.path)
-      ensure
-        file.close
-        file.unlink
+    context "when the file exists" do
+      it "returns a set of records with the contents of the file" do
+        file_parser = FileParser.new
+        file = Tempfile.new('FileParser_parse_file_test')
+        begin
+          file.puts("Jones Sarah Female 4/1/1982 Red")
+          file.puts("Smith Joe Male 5/15/1988 Blue")
+          file.rewind
+          result_set = file_parser.parse_file(file.path)
+        ensure
+          file.close
+          file.unlink
+        end
+
+        record1 = Record.new
+        record1.set_values_manually("Jones", "Sarah", "Female", "Red", "4/1/1982")
+
+        record2 = Record.new
+        record2.set_values_manually("Smith", "Joe", "Male", "Blue", "5/15/1988")
+
+        expect(result_set.length).to eq(2)
+        expect(result_set.include?(record1)).to be_true
+        expect(result_set.include?(record2)).to be_true
       end
-
-      record1 = Record.new
-      record1.set_values_manually("Jones", "Sarah", "Female", "Red", "4/1/1982")
-
-      record2 = Record.new
-      record2.set_values_manually("Smith", "Joe", "Male", "Blue", "5/15/1988")
-
-      expect(result_set.length).to eq(2)
-      expect(result_set.include?(record1)).to be_true
-      expect(result_set.include?(record2)).to be_true
+    end
+  end
+  context "when the file does not exist" do
+    it "rescues the exception" do
+      file_parser = FileParser.new
+      expect {file_parser.parse_file("non_existant_file.bar")}.to_not raise_error
     end
   end
 end
