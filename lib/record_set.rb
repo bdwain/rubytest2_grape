6a511044fe930a95dab373fab666efef7e6c7185
Bryan Wain <bryandwain@gmail.com>
2013-11-25 20:24:35 -0600

    added AddRecrods method to RecordSet

--- lib/record_set.rb
+++ lib/record_set.rb
@@ -1,3 +1,12 @@
 class RecordSet
+  attr_reader :records
+
+  def initialize
+    @records = Array.new
+  end
+
+  def AddRecords(newRecords)
+    @records.concat(newRecords.select {|record| record.valid? })
+  end
 
 end
--- spec/record_set_spec.rb
+++ spec/record_set_spec.rb
@@ -1,5 +1,32 @@
 require 'record_set'
+require 'record'
 
 describe RecordSet do
-  
+  describe "AddRecords" do
+    let(:records) do
+      record1 = Record.new
+      record1.set_values_manually("Smith", "Bob", "Male", "Blue", "5/15/1988")
+      record2 = Record.new
+      record2.set_values_manually("Jones", "Tom", "Male", "Green", "1/2/1990")
+      return [record1, record2]
+    end
+
+    let(:set) {RecordSet.new}
+
+    it "Adds the passed in records to the set" do
+      set.AddRecords(records)
+
+      expect(set.records.size).to eq(2)
+      expect(set.records.include?(records[0])).to be_true
+      expect(set.records.include?(records[1])).to be_true
+    end
+
+    it "Doesn't add invalid records to the set" do
+      record3 = Record.new
+      records.push(record3)
+
+      set.AddRecords(records)
+      expect(set.records.include?(record3)).to be_false
+    end
+  end
 end
