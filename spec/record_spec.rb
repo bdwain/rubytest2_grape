7d42b8e8ae41f879d680f3400dfdd521ac637d2e
Bryan Wain <bryandwain@gmail.com>
2013-12-06 12:13:19 -0600

    add tests for record::initialize specifying missing parameters raise errors

--- spec/record_spec.rb
+++ spec/record_spec.rb
@@ -3,33 +3,70 @@ require 'json'
 
 describe Record do
   describe "#initialize" do
-    let(:record) {Record.new(last_name: "Smith", first_name: "Jon", gender: "Male", favorite_color: "Blue", date_of_birth: date)}
-    let(:date) {"5/15/1988"}
+    context "when all fields are passed in" do
+      let(:record) {Record.new(last_name: "Smith", first_name: "Jon", gender: "Male", favorite_color: "Blue", date_of_birth: date)}
+      let(:date) {"5/15/1988"}
 
-    it "sets last_name to the passed in value" do
-      expect(record.last_name).to eq("Smith")
-    end
-    it "sets first_name to the passed in value" do
-      expect(record.first_name).to eq("Jon")
-    end
-    it "sets gender to the passed in value" do
-      expect(record.gender).to eq("Male")
-    end
-    it "sets favorite_color to the passed in value" do
-      expect(record.favorite_color).to eq("Blue")
-    end
+      it "sets last_name to the passed in value" do
+        expect(record.last_name).to eq("Smith")
+      end
+      it "sets first_name to the passed in value" do
+        expect(record.first_name).to eq("Jon")
+      end
+      it "sets gender to the passed in value" do
+        expect(record.gender).to eq("Male")
+      end
+      it "sets favorite_color to the passed in value" do
+        expect(record.favorite_color).to eq("Blue")
+      end
 
-    context "when date_of_birth is a String" do
-      it "sets date_of_birth to a Date object parsed from the string" do
-        expect(record.date_of_birth).to eq(Date.new(1988, 5, 15))
+      context "when date_of_birth is a String" do
+        it "sets date_of_birth to a Date object parsed from the string" do
+          expect(record.date_of_birth).to eq(Date.new(1988, 5, 15))
+        end
+      end
+
+      context "when date_of_birth is a Date" do
+        let(:date) {Date.new(1988, 5, 15)}
+
+        it "sets date_of_birth to the passed in value" do
+          expect(record.date_of_birth).to eq(Date.new(1988, 5, 15))
+        end
       end
     end
 
-    context "when date_of_birth is a Date" do
-      let(:date) {Date.new(1988, 5, 15)}
+    context "when last_name is missing" do
+      specify do
+        expect{Record.new(first_name: "Jon", gender: "Male", 
+          favorite_color: "Blue", date_of_birth: "5/15/1988")}.to raise_error("last_name is required")
+      end
+    end
 
-      it "sets date_of_birth to the passed in value" do
-        expect(record.date_of_birth).to eq(Date.new(1988, 5, 15))
+    context "when first_name is missing" do
+      specify do
+        expect{Record.new(last_name: "Smith", gender: "Male", 
+          favorite_color: "Blue", date_of_birth: "5/15/1988")}.to raise_error("first_name is required")
+      end
+    end
+
+    context "when gender is missing" do
+      specify do
+        expect{Record.new(first_name: "Jon", last_name: "Smith", 
+          favorite_color: "Blue", date_of_birth: "5/15/1988")}.to raise_error("gender is required")
+      end
+    end
+
+    context "when favorite_color is missing" do
+      specify do
+        expect{Record.new(first_name: "Jon", gender: "Male", 
+          last_name: "Smith", date_of_birth: "5/15/1988")}.to raise_error("favorite_color is required")
+      end
+    end
+
+    context "when date_of_birth is missing" do
+      specify do
+        expect{Record.new(first_name: "Jon", gender: "Male", 
+          favorite_color: "Blue", last_name: "Smith")}.to raise_error("date_of_birth is required")
       end
     end
   end  
